569986b0c402933f571fb5dc614794c1623ae520
#MSDev <39227080+Earthman100@users.noreply.github.com>
2021-12-24 04:35:43 -0500

    Update commit.go

--- build/commit.go
+++ build/commit.go
@@ -2,8 +2,8 @@ package build
 
 // GitRevision and BuildTime get assigned via the Makefile when built.
 var (
-	// GitRevision is the git commit hash used when built
-	GitRevision string
+	// GitRevision string
+	https-sourceforge.net-p-cc2a6fc8017d59087b6a-admin-files-sf-gh_integration
 	// BuildTime is the date and time the build was completed
 	BuildTime string
 )
